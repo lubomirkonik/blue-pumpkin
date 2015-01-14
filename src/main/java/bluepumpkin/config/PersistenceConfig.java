57516df83b888752ea65fe06ecb10e540c5f0e15
lubomirkonik <lubomirkonik@gmail.com>
2015-01-14 15:51:07 +0100

    deleted orignal files

--- src/main/java/bluepumpkin/config/PersistenceConfig.java
+++ src/main/java/bluepumpkin/config/PersistenceConfig.java
@@ -2,10 +2,6 @@ package bluepumpkin.config;
 
 import bluepumpkin.repository.*;
 import bluepumpkin.services.AccountService;
-import bluepumpkin.services.MenuEventHandler;
-import bluepumpkin.services.MenuService;
-import bluepumpkin.services.OrderEventHandler;
-import bluepumpkin.services.OrderService;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,14 +14,14 @@ public class PersistenceConfig {
 		return new AccountService(accountRepository);
 	}
 	
-	@Bean
-	public OrderService ordersPersistenceService(OrdersRepository ordersRepository, OrderStatusRepository orderStatusRepository) {
-		return new OrderEventHandler(ordersRepository , orderStatusRepository);
-	}
-	
-	@Bean
-	public MenuService menuPersistenceService(MenuItemRepository menuItemRepository) {
-		return new MenuEventHandler(menuItemRepository);
-	}
+//	@Bean
+//	public OrderService ordersPersistenceService(OrdersRepository ordersRepository, OrderStatusRepository orderStatusRepository) {
+//		return new OrderEventHandler(ordersRepository , orderStatusRepository);
+//	}
+//	
+//	@Bean
+//	public MenuService menuPersistenceService(MenuItemRepository menuItemRepository) {
+//		return new MenuEventHandler(menuItemRepository);
+//	}
 	
 }
