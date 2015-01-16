31d3039a909913175e6516315a76427ad57ca585
lubomirkonik <lubomirkonik@gmail.com>
2015-01-16 22:51:58 +0100

    Entity Classes

---
+++ src/main/java/bluepumpkin/domain/Team.java
@@ -0,0 +1,103 @@
+package bluepumpkin.domain;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author Lubosh
+ */
+@Entity(name = "team")
+public class Team {
+	
+    @Id
+    @Column(name = "ID")
+    private Integer id;
+    
+    @Column(name = "Score")
+    private Integer score;
+    
+    @ManyToMany(mappedBy = "teamList")
+    private List<Employee> employeeList;
+    
+    @JoinColumn(name = "EventID", referencedColumnName = "ID", nullable = false)
+    @ManyToOne(optional = false)
+    private Event eventID;
+
+    public Team() {
+    }
+
+    public Team(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    public Event getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(Event eventID) {
+        this.eventID = eventID;
+    }
+
+//    @Override
+//    public int hashCode() {
+//        int hash = 0;
+//        hash += (id != null ? id.hashCode() : 0);
+//        return hash;
+//    }
+//
+//    @Override
+//    public boolean equals(Object object) {
+//        // TODO: Warning - this method won't work in the case the id fields are not set
+//        if (!(object instanceof Team)) {
+//            return false;
+//        }
+//        Team other = (Team) object;
+//        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+//            return false;
+//        }
+//        return true;
+//    }
+
+    @Override
+    public String toString() {
+        return "bluepumpkin.domain.Team[ id=" + id + " ]";
+    }
+    
+}
+
---
+++ src/main/java/bluepumpkin/domain/web/AccountEmployee.java
@@ -0,0 +1,148 @@
+package bluepumpkin.domain.web;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import bluepumpkin.domain.Account;
+
+/**
+*
+* @author Lubosh
+*/
+public class AccountEmployee {
+
+   private static final String NOT_BLANK_MESSAGE = "The value may not be empty!";
+   private static final String EMAIL_MESSAGE = "The value must be a valid email!";
+
+//   in case of entity update
+   private Long id;
+   
+   @NotBlank(message = AccountEmployee.NOT_BLANK_MESSAGE)
+   @Email(message = AccountEmployee.EMAIL_MESSAGE)
+   private String email;
+   
+   @NotBlank(message = AccountEmployee.NOT_BLANK_MESSAGE)
+   private String password;
+   
+   @NotNull(message = AccountEmployee.NOT_BLANK_MESSAGE)
+   @NotEmpty(message = AccountEmployee.NOT_BLANK_MESSAGE)
+   private String firstName;
+   
+   @NotNull(message = AccountEmployee.NOT_BLANK_MESSAGE)
+   @NotEmpty(message = AccountEmployee.NOT_BLANK_MESSAGE)
+   private String lastName;
+
+//   TODO Checkbox
+   @NotNull(message = AccountEmployee.NOT_BLANK_MESSAGE)
+   @NotEmpty(message = AccountEmployee.NOT_BLANK_MESSAGE)
+   private String position;
+   
+//   TODO Checkbox
+   @NotNull(message = AccountEmployee.NOT_BLANK_MESSAGE)
+   @NotEmpty(message = AccountEmployee.NOT_BLANK_MESSAGE)
+   private String department;
+   
+//   TODO Set pattern for telephone number (eg.+421 900 900 900) in presentation layer
+   @NotNull(message = AccountEmployee.NOT_BLANK_MESSAGE)
+   @NotEmpty(message = AccountEmployee.NOT_BLANK_MESSAGE)
+   private String telephone;
+   
+//   TODO Set pattern for date (01.01.2001) in presentation layer    
+   @Past(message = "The value \"${formatter.format('%1$td.%1$tm.%1$tY', validatedValue)}\" is not in past!")
+   private Date dateOfBirth;
+
+   public AccountEmployee() {
+   }
+
+   public AccountEmployee(Long id) {
+       this.id = id;
+   }
+   
+   public Account createAccount() {
+       return new Account(getEmail(), getPassword(), "ROLE_USER");
+	}
+
+   public Long getId() {
+       return id;
+   }
+
+   public void setId(Long id) {
+       this.id = id;
+   }
+
+   public String getFirstName() {
+       return firstName;
+   }
+
+   public void setFirstName(String firstName) {
+       this.firstName = firstName;
+   }
+
+   public String getLastName() {
+       return lastName;
+   }
+
+   public void setLastName(String lastName) {
+       this.lastName = lastName;
+   }
+
+   public String getPosition() {
+       return position;
+   }
+
+   public void setPosition(String position) {
+       this.position = position;
+   }
+
+   public String getDepartment() {
+       return department;
+   }
+
+   public void setDepartment(String department) {
+       this.department = department;
+   }
+
+   public String getTelephone() {
+       return telephone;
+   }
+
+   public void setTelephone(String telephone) {
+       this.telephone = telephone;
+   }
+
+   public Date getDateOfBirth() {
+       return dateOfBirth;
+   }
+
+   public void setDateOfBirth(Date dateOfBirth) {
+       this.dateOfBirth = dateOfBirth;
+   }
+
+   public String getEmail() {
+       return email;
+   }
+
+   public void setEmail(String email) {
+       this.email = email;
+   }
+   
+   public String getPassword() {
+		return password;
+	}
+
+   public void setPassword(String password) {
+		this.password = password;
+   }
+   
+   @Override
+   public String toString() {
+       return "bluepumpkin.domain.web.AccountEmployee[ id=" + id + " ]";
+   }
+   
+}
